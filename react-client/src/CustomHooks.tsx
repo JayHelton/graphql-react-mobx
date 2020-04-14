9ca28f5214ce1d02f50e2437995010c311936669
jaydhelton <jaydhelton@gmail.com>
2020-04-14 07:41:26 -0400

    random thought

--- react-client/src/CustomHooks.tsx
+++ react-client/src/CustomHooks.tsx
@@ -1,11 +1,20 @@
+import { useQuery } from '@apollo/react-hooks';
 import { useEffect } from 'react';
 
 import { useStores } from './App';
+import { USERS_QUERY } from './Queries';
 
 export function useUserStore() {
     const { userStore } = useStores();
     useEffect(() => {
         userStore.fetch();
     }, []);
+    return userStore;
+}
+
+export function useUserStoreTwoPointOhYeah() {
+    const { userStore } = useStores();
+    const { data } = useQuery(USERS_QUERY);
+    userStore.setUsers(data?.users);
     return userStore;
 }
