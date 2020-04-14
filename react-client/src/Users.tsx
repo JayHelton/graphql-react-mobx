7476dfcae6c5054655219bac1ccd3ae27a4e96ae
jaydhelton <jaydhelton@gmail.com>
2020-04-14 07:34:06 -0400

    test

--- react-client/src/Users.tsx
+++ react-client/src/Users.tsx
@@ -4,30 +4,30 @@ import React, { useEffect } from 'react';
 import { useStores } from './App';
 
 export const User = observer(() => {
-  const { userStore } = useStores();
-  useEffect(() => {
-    userStore.fetch();
-  }, []);
-  return (
-    <>
-      <label>
-        Email:
-        <input
-          name="email"
-          value={userStore.searchCriteria?.email}
-          onChange={(e) =>
-            userStore.addSearchCriteria({
-              [e.target.name]: e.target.value,
-            })
-          }
-        />
-      </label>
+    const { userStore } = useStores();
+    useEffect(() => {
+        userStore.fetch();
+    }, []);
+    return (
+        <>
+            <label>
+                Email:
+                <input
+                    name="email"
+                    value={userStore.searchCriteria?.email}
+                    onChange={(e) =>
+                        userStore.addSearchCriteria({
+                            [e.target.name]: e.target.value,
+                        })
+                    }
+                />
+            </label>
 
-      {userStore.searchedUsers?.map((u: any) => (
-        <p>
-          {u.email} - {u.name}
-        </p>
-      ))}
-    </>
-  );
+            {userStore.searchedUsers?.map((u: any) => (
+                <p>
+                    {u.email} - {u.name}
+                </p>
+            ))}
+        </>
+    );
 });
