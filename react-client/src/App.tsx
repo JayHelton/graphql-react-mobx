30483f893bdbc58a01a9a1762eb108a210a298d6
jaydhelton <jaydhelton@gmail.com>
2020-04-14 07:34:42 -0400

    formatting

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -11,21 +11,21 @@ export const client = new ApolloClient({ uri: 'http://localhost:4000' });
 const userStore = new UserStore();
 
 export const userContext = React.createContext({
-	userStore,
+    userStore,
 });
 
 export const useStores = () => React.useContext(userContext);
 
 function App() {
-	return (
-		<ApolloProvider client={client}>
-			<div className="App">
-				<header className="App-header">GraphQL Fun</header>
-			</div>
-			<HelloWord></HelloWord>
-			<User></User>
-		</ApolloProvider>
-	);
+    return (
+        <ApolloProvider client={client}>
+            <div className="App">
+                <header className="App-header">GraphQL Fun</header>
+            </div>
+            <HelloWord></HelloWord>
+            <User></User>
+        </ApolloProvider>
+    );
 }
 
 export default App;
--- react-client/src/Store.tsx
+++ react-client/src/Store.tsx
@@ -4,41 +4,41 @@ import { client } from './App';
 import { USERS_QUERY } from './Queries';
 
 export class UserStore {
-	@observable
-	users = [];
+    @observable
+    users = [];
 
-	@observable
-	searchCriteria: any = {};
+    @observable
+    searchCriteria: any = {};
 
-	@action
-	addSearchCriteria(criteria: any) {
-		this.searchCriteria = { ...this.searchCriteria, ...criteria };
-	}
+    @action
+    addSearchCriteria(criteria: any) {
+        this.searchCriteria = { ...this.searchCriteria, ...criteria };
+    }
 
-	@action
-	clearSearch() {
-		this.searchCriteria = {};
-	}
+    @action
+    clearSearch() {
+        this.searchCriteria = {};
+    }
 
-	@action
-	setUsers(users: any) {
-		this.users = users;
-	}
+    @action
+    setUsers(users: any) {
+        this.users = users;
+    }
 
-	@action
-	async fetch() {
-		const { data } = await client.query({ query: USERS_QUERY });
-		this.users = data?.user;
-	}
+    @action
+    async fetch() {
+        const { data } = await client.query({ query: USERS_QUERY });
+        this.users = data?.user;
+    }
 
-	@computed
-	get searchedUsers() {
-		const { email } = this.searchCriteria;
-		if (email) {
-			return this.users.filter((u: { email: string }) =>
-				u.email.includes(email)
-			);
-		}
-		return this.users;
-	}
+    @computed
+    get searchedUsers() {
+        const { email } = this.searchCriteria;
+        if (email) {
+            return this.users.filter((u: { email: string }) =>
+                u.email.includes(email)
+            );
+        }
+        return this.users;
+    }
 }
